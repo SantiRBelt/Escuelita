26f45c0c9a71119ae8e137fbc208ff366e1f8bdc
SantiRBelt <srendon@ofima.com>
2025-02-14 16:16:18 -0500

    no exponer puerto 60

--- Entregable-Universities/obj/Debug/net8.0/Entregable-Universities.AssemblyInfo.cs
+++ Entregable-Universities/obj/Debug/net8.0/Entregable-Universities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entregable-Universities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2307741fea41902f1ebc952dfad1972db693e01f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d159586257318aef6333eda0cf00c920fcd3a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entregable-Universities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entregable-Universities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
